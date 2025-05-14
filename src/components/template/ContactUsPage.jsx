e8373395433a9dc960d8fdf83b77b42df9239d49
Mahsa Vafaei <mahsavafaei83@gmail.com>
2025-05-14 21:06:45 +0330

    redesign

--- src/components/template/ContactUsPage.jsx
+++ src/components/template/ContactUsPage.jsx
@@ -9,7 +9,7 @@ function ContactUsPage() {
       <h1 className="py-10 text-center text-3xl font-black text-darkColor">
         تماس با ما
       </h1>
-      <div className="mx-auto my-10 flex w-4/5 flex-col items-center gap-10 rounded-full bg-white px-4  shadow-xl lg:max-w-[1000px] lg:rounded-3xl">
+      <div className="mx-auto pt-10 my-10 flex w-4/5 flex-col items-center gap-10 rounded-full bg-white px-4  shadow-xl lg:max-w-[1000px] lg:rounded-3xl">
         <div className="group flex h-80 w-72 flex-col items-center justify-between gap-5 rounded-xl bg-lightColor/50 p-5 py-10 text-green-900 transition-all duration-300 ease-in-out hover:shadow-2xl">
           <div className="flex h-16 w-16 items-center justify-center rounded-[27px] bg-darkColor transition-all duration-300 ease-in-out group-hover:bg-white group-hover:border-solid border-darkColor/60 border-[1px]">
             <span className="text-3xl text-white group-hover:text-darkColor">
