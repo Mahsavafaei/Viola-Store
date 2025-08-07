7262b331ce838ff339c9c51f30dbadd9061130ce
Mahsa Vafaei <mahsavafaei83@gmail.com>
2025-08-07 23:47:33 +0330

    delete notfound page

--- src/app/not-found.js
+++
@@ -1,6 +0,0 @@
-import NotFoundPage from "@/components/modules/NotFoundPage";
-
-
-export default function NotFound() {
-  return <NotFoundPage />;
-}
--- src/components/modules/NotFoundPage.jsx
+++ src/components/modules/NotFoundPage.jsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+// import Image from "next/image";
 // import { NotFound } from "../../../public/index";
 
 function NotFoundPage() {
